8668640a6cace829807cdcccf503b66b7e356b25
cristian <cusumdt@gmail.com>
2019-10-30 15:28:43 -0300

    explosión menu

---
+++ Assets/Scripts/UI/Shake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+
+    public Animator m_Animator;
+
+
+    // Update is called once per frame
+    void ShakeCamera()
+    {
+        m_Animator.SetBool("On", true);
+    }
+}

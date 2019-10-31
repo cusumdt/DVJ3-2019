3bc7dde0dedaa8d014b489c05d598ae77cab1446
cristian <cusumdt@gmail.com>
2019-10-31 13:40:57 -0300

    Cursor locked

---
+++ Assets/CursorManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}

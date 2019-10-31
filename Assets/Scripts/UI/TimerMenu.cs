2d87a504352598d40de71c188df8a275aeab84e4
cristian <cusumdt@gmail.com>
2019-10-31 14:44:07 -0300

    Game Over Time

---
+++ Assets/Scripts/UI/TimerMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerMenu : MonoBehaviour
+{
+    float carga;
+    public float velCarga;
+    public GameObject menu;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (carga >= 100)
+        {
+
+            carga = 0;
+            menu.SetActive(true);
+     
+        }
+        else
+        {
+            carga = carga + (Time.deltaTime * velCarga);
+
+        }
+    }
+}

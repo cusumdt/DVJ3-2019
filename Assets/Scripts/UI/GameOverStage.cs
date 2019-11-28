964a00e136fbce254811649167afd6d5dc05eecf
cristian <cusumdt@gmail.com>
2019-11-28 12:20:49 -0300

    GameOverStage fixed

--- Assets/Scripts/UI/GameOverStage.cs
+++ Assets/Scripts/UI/GameOverStage.cs
@@ -7,21 +7,12 @@ public class GameOverStage : MonoBehaviour {
     public Image seccond;
     public SpriteRenderer thisWinner;
     public SpriteRenderer thisLose;
-    bool init;
     void Start () {
-        init = false;
-
-    }
-
-    void Update () {
-        if (!init) {
-            GameObject asd = GameManager.Get ().GetLose ();
-            GameObject ase = GameManager.Get ().GetWinner ();
-            thisLose = asd.GetComponent<SpriteRenderer> ();
-            thisWinner = ase.GetComponent<SpriteRenderer> ();
-            seccond.sprite = thisLose.sprite;
-            winner.sprite = thisWinner.sprite;
-            init = true;
-        }
+        GameObject Loser = GameManager.Get().GetLose();
+        GameObject Winner = GameManager.Get().GetWinner();
+        thisLose = Loser.GetComponent<SpriteRenderer>();
+        thisWinner = Winner.GetComponent<SpriteRenderer>();
+        seccond.sprite = thisLose.sprite;
+        winner.sprite = thisWinner.sprite;
     }
 }

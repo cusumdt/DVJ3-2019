3c947478eddf3123b4ab07770980b6afc7cf8f39
cristian <cusumdt@gmail.com>
2019-11-28 12:33:44 -0300

    Skip Intro

--- Assets/Scripts/UI/Inicio.cs
+++ Assets/Scripts/UI/Inicio.cs
@@ -6,4 +6,10 @@ public class Inicio : MonoBehaviour {
     void PlayMenu () {
         SceneManager.LoadScene ("Menu");
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape") || Input.GetKeyDown("joystick 1 button 7") || Input.GetKeyDown("joystick 2 button 7"))
+            SceneManager.LoadScene("Menu");
+    }
 }

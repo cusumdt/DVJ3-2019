276e62395a71cf10078df9b232e0f5e05596266a
cristian <cusumdt@gmail.com>
2019-12-04 04:58:07 -0300

    RandomLevel

---
+++ Assets/Scripts/Utility/EnvironmentManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    public List<GameObject> Environment;
+    public int EnviroAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range (0 , EnviroAmount);
+        Environment[rand].SetActive(true);         
+
+    }
+
+}

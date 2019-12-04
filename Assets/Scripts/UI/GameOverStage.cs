a789b57ffd8c81befbfa1b908fc22ae3c91adfcb
cristian <cusumdt@gmail.com>
2019-12-04 04:04:19 -0300

    Animación de victoria rony y wambo

--- Assets/Scripts/UI/GameOverStage.cs
+++ Assets/Scripts/UI/GameOverStage.cs
@@ -7,11 +7,14 @@ public class GameOverStage : MonoBehaviour {
     public Image seccond;
     public SpriteRenderer thisWinner;
     public SpriteRenderer thisLose;
+    public Animator WinnerAnim;
+
     void Start () {
         GameObject Loser = GameManager.Get().GetLose();
         GameObject Winner = GameManager.Get().GetWinner();
         thisLose = Loser.GetComponent<SpriteRenderer>();
         thisWinner = Winner.GetComponent<SpriteRenderer>();
+        WinnerAnim.SetInteger("Player", Winner.name == "player" ? 1 : 2);
         seccond.sprite = thisLose.sprite;
         winner.sprite = thisWinner.sprite;
     }
